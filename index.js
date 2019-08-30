15a55dd04c8e0a42e323516c20d380bdfee3eeb2
shariff94 <simplyshariff@gmail.com>
2019-08-30 14:44:08 +0530

    web cam and classification, using KNN

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const webcamElement = document.getElementById('webcam');
+const classifier = knnClassifier.create();
 let net;
 
 async function app() {
@@ -9,16 +10,37 @@ async function app() {
   console.log('Sucessfully loaded model');
 
   await setupWebcam();
+
+  // Reads an image from the webcam and associates it with a specific class
+  // index.
+  const addExample = classId => {
+    // Get the intermediate activation of MobileNet 'conv_preds' and pass that
+    // to the KNN classifier.
+    const activation = net.infer(webcamElement, 'conv_preds');
+
+    // Pass the intermediate activation to the classifier.
+    classifier.addExample(activation, classId);
+  };
+
+  // When clicking a button, add an example for that class.
+  document.getElementById('class-a').addEventListener('click', () => addExample(0));
+  document.getElementById('class-b').addEventListener('click', () => addExample(1));
+  document.getElementById('class-c').addEventListener('click', () => addExample(2));
+
   while (true) {
-    const result = await net.classify(webcamElement);
+    if (classifier.getNumClasses() > 0) {
+      // Get the activation from mobilenet from the webcam.
+      const activation = net.infer(webcamElement, 'conv_preds');
+      // Get the most likely class and confidences from the classifier module.
+      const result = await classifier.predictClass(activation);
 
-    document.getElementById('console').innerText = `
-      prediction: ${result[0].className}\n
-      probability: ${result[0].probability}
-    `;
+      const classes = ['A', 'B', 'C'];
+      document.getElementById('console').innerText = `
+        prediction: ${classes[result.classIndex]}\n
+        probability: ${result.confidences[result.classIndex]}
+      `;
+    }
 
-    // Give some breathing room by waiting for the next animation frame to
-    // fire.
     await tf.nextFrame();
   }
 }
